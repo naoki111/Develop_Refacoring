789838150866add64761fcc71c950516917b038b
naoki111 <2004240028yn@edu.oca.ac.jp>
2022-12-17 15:10:21 +0900

    1217

---
+++ str.py
@@ -0,0 +1,7 @@
+def print_str(str ,height_number ,width_number):
+    str = "aaa"
+    height_number = 3
+    width_number  = 3
+    for i in height_number:
+        for i in width_number:
+            print (str)
