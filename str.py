231ca3545f4c022d5e0cd97ff5820f3bead6d7e9
naoki111 <2004240028yn@edu.oca.ac.jp>
2022-12-17 16:47:54 +0900

    1217

--- str.py
+++ str.py
@@ -1,5 +1,15 @@
 import numpy
 def print_string(str ,height_number ,width_number):
+    """_summary_
+    1つ目の数値で縦、２つ目の数値で横に繰り返す文字列を返却する関数
+    Args:
+        str (_type_): ５文字以上１０文字以下の文字列
+        height_number (_type_): 縦に繰り返す数を決める数値
+        width_number (_type_): 横に繰り返す数を決める数値
+
+    Returns:
+        _type_: _description_
+    """
     height_number = int(height_number)
     width_number = int(width_number)
     for i in range(height_number):
