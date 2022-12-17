d9676c8687dde5ba4908841bd347039c273624df
naoki111 <2004240028yn@edu.oca.ac.jp>
2022-12-17 16:08:04 +0900

    1217

--- str.py
+++ str.py
@@ -1,11 +1,10 @@
-import random
+import numpy
 def print_string(str ,height_number ,width_number):
-    str = "aaa"
-    height_number = random.randint(0,10)
-    width_number  = random.randint(0,10)
-    for i in height_number:
-        for i in width_number:
-            print (str)
+    height_number = int(height_number)
+    width_number = int(width_number)
+    for i in range(height_number):
+        for i in range(width_number):
+            return str
 
 if __name__ == "__main__":
-    print_string('abc', 1 , 2)
+    print_string('abc', numpy.random.randint(0,10), numpy.random.randint(0,10))
